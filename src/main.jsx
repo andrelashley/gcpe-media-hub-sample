ce9cbda61a63767198ca460df9145703b6847adf
André Lashley <andre.lashley@gmail.com>
2025-04-30 16:45:31 -0700

    Install Fluent UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <App />
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
   </StrictMode>,
 )
